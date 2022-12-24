e80c5435d5660028f3f90b1a4a70577ad80405b6
Irina <irina@netmagik.com>
2022-12-24 16:15:14 -0500

    Update error message

--- src/ErrorComponent.js
+++ src/ErrorComponent.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 const ErrorComponent = () => {
   return (
-    // <h3 className='results'>API either reached maximum requests on a Free Plan, or there is no data provided for the selected code</h3>
     <h3 className='results'>Data does not exist for this currency symbol</h3>
   )
 }
