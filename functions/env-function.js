1deff2951fc5b418b0ecf619da8c0f7b71b96ae7
Irina <irina@netmagik.com>
2022-12-27 15:09:42 -0500

    Add serverless function

--- functions/env-function.js
+++ functions/env-function.js
@@ -1,8 +1,30 @@
-const { crypto } = process.env.REACT_APP_API_KEY;
+const fetch = require("node-fetch");
+//const BASE_URL = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${query}&to_currency=USD&apikey=${key}";
+const BASE_URL = "https://www.alphavantage.co/";
 
 exports.handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: crypto,
-  };
+  // Access the environment variables using process.env
+  try {
+    const { query } = event.queryStringParameters;
+    return fetch(
+      `${BASE_URL}/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${query}&to_currency=USD&apikey=${process.env.API_KEY}`
+    )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response not ok");
+        }
+        return response.json();
+      })
+      .then((json) => {
+        return {
+          statusCode: 200,
+          body: JSON.stringify(json),
+        };
+      });
+  } catch (error) {
+    return {
+      statusCode: 404,
+      body: error.toString(),
+    };
+  }
 };
